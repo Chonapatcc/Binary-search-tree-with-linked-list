c8f5174108a68ae66dd2a0fd6dc7ac636d88a07b
Chonapatcc <Chonapatcz@gmail.com>
2024-01-28 22:50:03 +0700

    bro

--- bro.cpp
+++ bro.cpp
@@ -5,19 +5,58 @@ struct node
     int val;
     struct node *left,*right;
 };
-
+int recur(int value,int h,struct node *x )
+{
+    h++;
+    cout <<"|"<< h<<"|" << endl;
+    if(x==NULL)
+    {
+        cout << "what" <<endl;
+        x=(struct node*)malloc(sizeof(struct node));
+        x->val = value;
+        x->left=x->right=NULL ;
+        return h;
+    }
+    else
+    {
+        if(value>(x->val)) //right
+        {
+            x=x->right;
+        }
+        else //left
+        {
+            x=x->left;
+        }
+        return recur(value,h,x);
+    }
+}
 int main()
 {
     int n;
     cin>>n;
-    int x;
-    cin>>x;
-    struct node *root = (struct node * )malloc(sizeof(struct node));
-    for(int i=1 ; i< n ; i++)
+    
+    struct node *root =(struct node*)malloc(sizeof(struct node));
+    int maxh =1 ;
+    
+    for(int i=0 ; i< n ; i++)
     {
-        
-        cin>>x;
-        
+        int h=0;
+        int val; 
+        cin>>val;
+        if(i==0)
+        {
+            root->val=val;
+            root->left=NULL;
+            root->right=NULL;
+            continue;
+        }
+        h= recur(val ,h,root);
+        if(h>maxh)
+        {
+            maxh = h;
+        }
 
+        
     }
+    cout << maxh;
 }
---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int val=1; // ? for what
+    struct node *left,*right;
+};
+
+int main()
+{
+    struct node *a , *b;
+
+    a = (struct node *)malloc(sizeof(struct node));
+    a->val = 1;
+    a->left=a->right =  NULL;
+
+    cout << a->val<<endl;
+     
+    if(a->right ==NULL)
+    {
+        cout << "NULL2" ;
+    }
+}
